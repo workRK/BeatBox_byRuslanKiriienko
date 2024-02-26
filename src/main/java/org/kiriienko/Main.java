bcd65d67d1eaeff65d25146b9ee5420e2377066b
workRK <rk29work@proton.me>
2024-02-26 18:39:58 +0200

    Make version 1.0

---
+++ src/main/java/org/kiriienko/Main.java
@@ -0,0 +1,11 @@
+package org.kiriienko;
+
+import org.kiriienko.app.MyBeatBox;
+
+public class Main {
+    public static void main(String[] args) {
+
+        new MyBeatBox().buildGUI();
+
+    }
+}
